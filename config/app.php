5b80a4a479f2aea4b84fa19c69d5a030cf7692a2
Swai-D <davyswai@gmail.com>
2019-12-04 03:36:52 -0800

    New Video
    Third Commit

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Unicodeveloper\Emoji\EmojiServiceProvider::class
+
 
         /*
          * Package Service Providers...
